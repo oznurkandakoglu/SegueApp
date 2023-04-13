8b260199c9605618c86ee69448c92bb9a4c384b6
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-04-13 14:09:27 +0300

    Initial Commit

---
+++ SegueAppUITests/SegueAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SegueAppUITestsLaunchTests.swift
+//  SegueAppUITests
+//
+//  Created by Tiga  on 13.04.2023.
+//
+
+import XCTest
+
+final class SegueAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
